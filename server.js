718736f21db189e8531ba9d622e4d79496c51de5
Monroe <me_menglu@163.com>
2021-08-15 16:01:29 +0800

    效果已实现

--- server.js
+++ server.js
@@ -22,15 +22,32 @@ var server = http.createServer(function(request, response) {
 
   console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
-  if (path === "/") {
+  
     response.statusCode = 200;
-    response.setHeader("Content-Type", "text/html;charset=utf-8");
-      const y = path;
-      response.write(fs.readFileSync(`./public${y}`));
+    //默认首页
+    const filePath = path === "/" ? "/index.html" : path
+    const index = filePath.lastIndexOf(".");
+    const suffix = filePath.substring(index);
+    const fileTypes = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "text/javascript",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+    };
+    response.setHeader("Content-Type", `${fileTypes[suffix]} || "text/html";charset=utf-8`)
+  
+    let content
+    try { content =fs.readFileSync(`./public${filePath}`)
+        
+    } catch (error) {
+        content = "文件不存在"
+         response.statusCode = 404; 
+    }
+      response.write(content);
       
     response.end();
-  }
- 
+
 
 
   /******** 代码结束，下面不要看 ************/
